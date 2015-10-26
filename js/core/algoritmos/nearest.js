168ce2ad5991300ecc86f0b8bcf689dd25f28ee9
ldavid <lucasolivdavid@gmail.com>
2015-10-26 18:21:51 -0200

    Update website

---
+++ js/core/algoritmos/nearest.js
@@ -0,0 +1,40 @@
+Grafo.Algoritmos.nearestNeighbor = function() {
+    lnConsole.message('K-Nearest-neighbor Search');
+
+    var nodes = Grafo.MatrizAdj.length;
+    var k = 3;
+
+    m = new Array(nodes);
+    for (var i = 0; i < nodes; i++) {
+        m[i] = new Array(nodes);
+        for (var j = 0; j < nodes; j++) m[i][j] = 0;
+    }
+
+    for (var node = 0; node < nodes; node++) {
+        neighborhood = kNearestNeighborsOf(Grafo.MatrizAdj, node, k);
+        console.log(neighborhood.length);
+
+        for (var i = 0; i < neighborhood.length; i++) {
+            neighbor = neighborhood[i][0];
+            cost = neighborhood[i][1];
+
+            m[neighbor][i] = m[i][neighbor] = cost;
+        }
+    }
+
+    Grafo.Algoritmos.MatrizAdj = m;
+    App(true);
+    return false;
+}
+
+function kNearestNeighborsOf(m, node, k) {
+    var neighborhood = [];
+
+    for (var i = 0; i < m[node].length; i++)
+        neighborhood[i] = [i, m[node][i]];
+
+    return neighborhood
+        .filter(function (e) { return e[1] != 0; })
+        .sort(function(left, right) { return Math.sign(left[1] - right[1]); })
+        .slice(0, k);
+}
